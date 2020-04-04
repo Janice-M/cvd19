3f562f0b23168cec404a07626a9e72b2fc733579
Hillkim <hillkim@protonmail.com>
2020-04-04 22:38:37 +0300

    setup and initial configuration

---
+++ src/components/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Components = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default Components;
+
+const styles = StyleSheet.create({});
---
+++ src/layouts/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Layouts = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default Layouts;
+
+const styles = StyleSheet.create({});
