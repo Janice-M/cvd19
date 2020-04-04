5664a98aae4d681330a7badea5ddb499bbc343f3
Hillkim <hillkim@protonmail.com>
2020-04-04 23:58:44 +0300

    :tada: Refactor theme context

--- src/themes/index.tsx
+++ src/themes/index.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import {light, dark} from '@eva-design/eva';
-import {cvdTheme} from './cvd19-theme';
-/**
- * theme types
- */
-export type Ttheme = 'light' | 'dark' | 'kenti';
+import {cvdTheme as cvd19} from './cvd19-theme';
 
-//app themes
-export const Themes = {
-  light: light,
-  dark: dark,
-  cvd19: cvdTheme,
+export const themes = {
+  light,
+  dark,
+  cvd19,
 };
 /**
  * provides theme and toggleTheme handler
  */
-export const ThemeContext = React.createContext({toggleTheme: () => {}});
+export const ThemeContext = React.createContext({
+  theme: 'cvd19',
+  toggleTheme: () => {},
+});
