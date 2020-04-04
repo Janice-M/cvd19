2d0ffe19a4d881cb87172fa6c439a0ac09637d80
Hillkim <hillkim@protonmail.com>
2020-04-05 02:45:13 +0300

    :wrench: Add inital screens

---
+++ src/screens/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Layout, Text} from '@ui-kitten/components';
+
+export const UsersScreen = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category="h1">USERS</Text>
+  </Layout>
+);
+
+export const OrdersScreen = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category="h1">ORDERS</Text>
+  </Layout>
+);
+export const AnotherScreen = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category="h1">Another</Text>
+  </Layout>
+);
