0c8cc327a171ddf330831739f8ddc89f55804774
Hillkim <hillkim@protonmail.com>
2020-04-05 04:06:57 +0300

    :lipstick: Update menu Icons

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,33 +2,28 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-
 import {
   BottomNavigation,
   BottomNavigationTab,
   Layout,
-  Icon,
 } from '@ui-kitten/components';
-
-import {UsersScreen, OrdersScreen, AnotherScreen} from '../screens';
+import {HomeScreen, CvdScreen, InfoScreen} from '../screens';
+import {Icon} from '../components';
 
 const BottomTab = createBottomTabNavigator();
-
-//icons
 
 //@ts-ignore
 const BottomTabBar = ({navigation, state}) => {
   const onSelect = (index: React.ReactText) => {
     navigation.navigate(state.routeNames[index]);
   };
-
   return (
     <Layout>
       <SafeAreaView>
         <BottomNavigation selectedIndex={state.index} onSelect={onSelect}>
-          <BottomNavigationTab title="Home" />
-          <BottomNavigationTab title="Cvd19" />
-          <BottomNavigationTab title="World" />
+          <BottomNavigationTab title="Home" icon={Icon.Home} />
+          <BottomNavigationTab title="Cvd-19" icon={Icon.Virus} />
+          <BottomNavigationTab title="Info" icon={Icon.Stats} />
         </BottomNavigation>
       </SafeAreaView>
     </Layout>
@@ -40,9 +35,9 @@ const TabNavigator = () => (
     tabBar={(
       props: JSX.IntrinsicAttributes & {navigation: any; state: any},
     ) => <BottomTabBar {...props} />}>
-    <BottomTab.Screen name="Users" component={UsersScreen} />
-    <BottomTab.Screen name="Orders" component={OrdersScreen} />
-    <BottomTab.Screen name="Another" component={AnotherScreen} />
+    <BottomTab.Screen name="Home" component={HomeScreen} />
+    <BottomTab.Screen name="Cvd19" component={CvdScreen} />
+    <BottomTab.Screen name="Info" component={InfoScreen} />
   </BottomTab.Navigator>
 );
 
