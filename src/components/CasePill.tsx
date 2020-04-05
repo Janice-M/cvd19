87433e005e3cf5e5d37088ca2da77b7a1c6ce42b
Hillkim <hillkim@protonmail.com>
2020-04-05 16:48:40 +0300

    :lipstick: Add pill styles,fix pill props,cleanup :tada:

--- src/components/CasePill.tsx
+++ src/components/CasePill.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import {Layout, Text} from '@ui-kitten/components';
-import {StyleSheet} from 'react-native';
+import {ViewStyle, StyleProp} from 'react-native';
 
 type pillType = 'deaths' | 'active' | 'critical' | 'recovered';
 
 interface CasePillProps {
+  /**
+   * number of people
+   */
   value: number;
+  /**
+   * type of case can be `deaths` | `active` | `critical` | `recovered`
+   */
   type: pillType;
 }
 
@@ -17,12 +23,26 @@ const colors = {
 };
 
 const CasePill: React.FC<CasePillProps> = ({value, type}) => {
+  const color = colors[type];
+  const caseStyles: StyleProp<ViewStyle> = {
+    flexGrow: 1,
+    borderRadius: 5,
+    flexDirection: 'row',
+    borderWidth: 2,
+    borderColor: color,
+    padding: 3,
+    alignItems: 'center',
+    marginTop: 5,
+    marginHorizontal: 3,
+    borderLeftWidth: 10,
+    justifyContent: 'space-between',
+  };
   return (
-    <Layout style={styles.case}>
-      <Text style={{marginRight: 5}} category="h5">
+    <Layout style={caseStyles}>
+      <Text style={{marginRight: 5, color: color}}>
         {type.toLocaleUpperCase()}
       </Text>
-      <Text status="primary" category="h5">
+      <Text style={{color: color}} category="h6">
         {value.toString()}
       </Text>
     </Layout>
@@ -30,11 +50,3 @@ const CasePill: React.FC<CasePillProps> = ({value, type}) => {
 };
 
 export default CasePill;
-
-const styles = StyleSheet.create({
-  case: {
-    borderRadius: 5,
-    flexDirection: 'row',
-    borderWidth: 3,
-  },
-});
